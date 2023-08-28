7ac2094c3b29b04844d70aa4d0760ae39fb051af
1kivanc <kivanc0556@gmail.com>
2023-08-28 11:32:09 +0300

    üçgen cizme

---
+++ kolayProjeler/ucgenCizme/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ucgenCizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cizlecek üçgenin boyutu ne kadar olsun: ");
+            int boyut = Convert.ToInt32(Console.ReadLine());
+
+            UcgenCizme ucgenCizme = new UcgenCizme();
+            ucgenCizme.UcgenCiz(boyut);
+            Console.ReadLine();
+        }
+    }
+
+    class UcgenCizme 
+
+    {
+        public void UcgenCiz(int boyut)
+        {
+            for(int i = 1; i<=boyut; i++)
+            {
+                for(int j=1; j<=i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+
+
+    }
+}
