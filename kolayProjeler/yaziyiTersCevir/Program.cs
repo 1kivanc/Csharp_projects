b6ba59ccd0f8e66ae20f9fb5aef71c612530d05e
1kivanc <kivanc0556@gmail.com>
2023-08-29 16:51:47 +0300

    swappedText

---
+++ kolayProjeler/yaziyiTersCevir/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yaziyiTersCevir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir string ifade girin:");
+            string input = Console.ReadLine();
+
+            string swappedText = SwapCharacters(input);
+            Console.WriteLine("Sonuç: " + swappedText);
+            Console.ReadLine();
+        }
+
+        static string SwapCharacters(string input)
+        {
+            char[] charArray = input.ToCharArray();
+
+            for (int i = 0; i < charArray.Length - 1; i += 2)
+            {
+                char temp = charArray[i];
+                charArray[i] = charArray[i + 1];
+                charArray[i + 1] = temp;
+            }
+
+            return new string(charArray);
+        }
+
+
+
+    }
+}
