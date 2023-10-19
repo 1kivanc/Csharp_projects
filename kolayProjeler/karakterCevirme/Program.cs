c4f09e5e4587180a700ad96c95ed69d9c2c52a40
1kivanc <kivanc0556@gmail.com>
2023-10-19 13:13:18 +0300

    karakter cevirme

---
+++ kolayProjeler/karakterCevirme/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karakterCevirme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Karakter ters çevirme");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Lütfen bir metin giriniz: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                string metin = Console.ReadLine();
+
+                if(metin == "")
+                {
+                    Console.WriteLine("Hiçbir metin girmediniz");
+                    continue;
+                }
+                string sonuc = karakterCevirme(metin);
+                Console.WriteLine(sonuc);
+
+            }
+
+
+        }
+        
+        static string karakterCevirme(string input)
+        {
+            char ilkKarakter = input[0];
+
+            char sonKarakter = input[input.Length - 1];
+
+
+            char[] metinKarakter = input.ToCharArray();
+
+            metinKarakter[0] = sonKarakter;
+            metinKarakter[input.Length - 1] = ilkKarakter;
+
+            return new string(metinKarakter);
+
+
+           
+        }
+
+
+
+    }
+}
